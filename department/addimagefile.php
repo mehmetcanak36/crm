ec7724b07655b7699df8203d9b9d38bcd81dbaa2
mehmet <mehmet.canak.36@gmail.com>
2020-12-15 10:50:30 +0300

    dosya işlemleri

--- department/addimagefile.php
+++ department/addimagefile.php
@@ -2,42 +2,42 @@
 $target_dir = "uploadsfiles/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));//dosya uzantısı bulma
 
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    echo "Dosya bir resimdir - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    echo "Dosya bir resim değil.";
     $uploadOk = 0;
   }
 }
 
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  echo "Üzgünüz, dosya zaten var.";
   $uploadOk = 0;
 }
 
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
+  echo "Üzgünüz, dosyanız çok büyük.";
   $uploadOk = 0;
 }
 
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  echo "Üzgünüz, sadece JPG, JPEG, PNG ve GIF dosyalarına izin verilmektedir.";
   $uploadOk = 0;
 }
 
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+  echo "Üzgünüz, dosyanız yüklenmedi.";
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    echo "Dosya ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " yüklendi.";
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    echo "Üzgünüz, dosyanızı yüklerken bir hata oluştu.";
   }
 }
 ?>
